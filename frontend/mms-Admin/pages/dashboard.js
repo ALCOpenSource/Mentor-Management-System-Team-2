c6d3c645ade5fdfdaf0ad7bffa88b284449742ac
Austin Asoluka <stracool9@gmail.com>
2023-05-31 03:55:50 +0100

    reworking dashboard screen with tailwind css

--- frontend/mms-Admin/pages/dashboard.js
+++ frontend/mms-Admin/pages/dashboard.js
@@ -1,13 +1,11 @@
-import React, { Component, useEffect, useState } from "react";
+import React from "react";
 import DetailsCard from "../components/atoms/DetailsCard";
-import { ReportCard } from "../components/atoms/DetailsCard";
-import { Col, Row } from "antd";
 import styles from "styles/admin/dashboard.module.scss";
 import { getDashboardData } from "./api/dashboard/index";
 import { fetchPrograms } from "pages/api/program";
 import { Button } from "../components/atoms/Button";
+import { Icons } from "../components/atoms/Icons";
 import { useQuery } from "@tanstack/react-query";
-import moment from "moment";
 import { Loader } from "components/Loader";
 import Link from "next/link";
 const createArray = (length) => [...Array(length)];
@@ -27,55 +25,54 @@ function Dashboard() {
 
   return (
     <>
-      <div className={styles.header_text}>Dashboard</div>
-
-      <Row>
-        <Col xs={12} lg={4} className={styles.active_program}>
-          <div className={styles.active_program_number}>
-            {data?.data?.active_programs}
+      <div className={`my-2 ${styles.header_text}`}>Dashboard</div>
+      <div className="flex gap-x-8">
+        <div className="px-3 pt-4 bg-mms-teal text-white rounded w-1/6">
+          <div className="flex justify-end">
+            <Button variant="white" size="small">
+              View
+            </Button>
           </div>
-
-          <div className={styles.active_program_p}>
-            Active
-            <span className={styles.active_program_view}>View</span>
-            <br /> Programs
+          <div className="flex items-center gap-x-4">
+            <h1 className="text-7xl">6</h1>
+            <h3 className="text-xl font-bold w-1/2 leading-5">
+              Active Programs
+            </h3>
           </div>
-        </Col>
+        </div>
 
-        <Col
-          xs={24}
-          sm={24}
-          md={24}
-          lg={19}
-          className={styles.details_card_container}>
-          <DetailsCard
-            icon="Person"
-            text="Mentors"
-            number={data?.data?.mentors}
-            marginRight="20px"
+        <div className="flex gap-x-4 justify-between p-3 w-5/6 bg-mms-ts-teal rounded">
+          <ReportCard
+            title="Mentors"
+            meta1="30"
+            meta2="10%"
+            icon={<Icons name="mentor-lg" />}
           />
-          <DetailsCard
-            icon="People"
-            text="Mentor Managers"
-            number={data?.data?.mentor_managers}
-            width="224px"
-            height="69px"
-            marginRight="15px"
+
+          <ReportCard
+            title="Mentor Managers"
+            meta1="30"
+            meta2="10%"
+            icon={<Icons name="mentor-manager-lg" />}
           />
-          <DetailsCard
-            icon="Task"
-            text="Tasks"
-            number={data?.data?.tasks}
-            marginRight="15px"
+
+          <ReportCard
+            title="Tasks"
+            meta1="30"
+            meta2="10%"
+            icon={<Icons name="task" fill="#058B94" />}
           />
-          <DetailsCard
-            icon="Report"
-            text="Reports"
-            number={data?.data?.reports}
+
+          <ReportCard
+            title="Reports"
+            meta1="30"
+            meta2="10%"
+            icon={<Icons name="report-sheet" width="35" />}
           />
-        </Col>
-      </Row>
-      <Row className={styles.details_card_row}>
+        </div>
+      </div>
+
+      {/* <Row className={styles.details_card_row}>
         <Col span={24} className={styles.programs_header_justify}>
           <div className={styles.overview_header}>Programs Overview</div>
           <div>
@@ -137,12 +134,12 @@ function Dashboard() {
         </Col>
       </Row>
 
-      <div className={styles.details_card_row} >
+      <div className={styles.details_card_row}>
         <Col span={24} className={styles.programs_header_justify}>
           <p className={styles.overview_header}>Tasks Overview</p>
         </Col>
-        <Row  span={24}>
-        <Col span={3} className={styles.details_in_progress_container}>
+        <Row span={24}>
+          <Col span={3} className={styles.details_in_progress_container}>
             <div className={styles.details_in_progress_header}>In Progress</div>
           </Col>
           <Col span={1}></Col>
@@ -169,7 +166,6 @@ function Dashboard() {
             </Link>
           </Col>
         </Row>
-
 
         <Row className={styles.mt}>
           <Col span={3} className={styles.details_in_progress_container}>
@@ -199,8 +195,26 @@ function Dashboard() {
             </Link>
           </Col>
         </Row>
-      </div>
+      </div> */}
     </>
+  );
+}
+
+function ReportCard({ flip, title, meta1, meta2, icon }) {
+  return (
+    <div
+      className={`flex justify-between items-center bg-mms-light-teal basis-1/2 rounded p-4 ${
+        flip && "flex-row-reverse"
+      }`}>
+      <div>
+        <p className="text-xl font-bold text-gray-600">{title}</p>
+        <div className="flex items-center gap-x-4">
+          <p className="text-lg font-semibold">{meta1}</p>{" "}
+          {meta2 && <p>{meta2}</p>}
+        </div>
+      </div>
+      {icon}
+    </div>
   );
 }
 
